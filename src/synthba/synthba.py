4bb91e36b704db459e4b003d139c5ab16af4821c
Lemuel <lemuelpuglisi001@gmail.com>
2025-10-23 00:08:31 +0200

    feat: download templates from HF

--- src/synthba/synthba.py
+++ src/synthba/synthba.py
@@ -1,6 +1,7 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Callable
 
 import pandas as pd
+import nibabel as nib
 import torch
 from torch.utils.data import DataLoader
 from monai import transforms
@@ -10,6 +11,9 @@ from nibabel.nifti1 import Nifti1Image
 from nibabel.nifti2 import Nifti2Image
 from huggingface_hub import hf_hub_download
 
+from .registration import align
+from .skull_stripping import SkullStripping
+from synthba import skull_stripping
 
 
 class SynthBA:
@@ -21,8 +25,12 @@ class SynthBA:
         self,
         device: str,
         checkpoint: Optional[str] = None,
-        model_type: str = 'g'
+        model_type: str = 'g',
+        skull_stripping: Optional[SkullStripping] = None,
+        align_fn: Optional[Callable] = None,
     ):
+        """
+        """
         if checkpoint is None and model_type not in ['u', 'g']:
             raise Exception('SynthBA `model_type` should be either `u` or `g`')
         
@@ -36,6 +44,16 @@ class SynthBA:
         self.model = DenseNet201(3, 1, 1, dropout_prob=0)
         self.model.load_state_dict(checkpoint)
         self.model = self.model.to(device).eval()
+
+        # Prepare the preprocessing methods. We allow users to provide different functions using composition.
+        self.skull_stripping = SkullStripping(self.device) if skull_stripping is None else skull_stripping
+        self.align_fn = align if align_fn is None else align_fn
+
+        # download the templates. Input scans will be aligned to one of these depeding on their weighting
+        self.templates = {
+            't1': nib.load(hf_hub_download(repo_id='lemuelpuglisi/synthba', filename='MNI152_T1_1mm_Brain.nii.gz')),
+            't2': nib.load(hf_hub_download(repo_id='lemuelpuglisi/synthba', filename='MNI152_T2_1mm_Brain.nii.gz'))
+        }
 
         self.transforms_fn_direct = transforms.Compose([
             transforms.EnsureChannelFirst(channel_dim='no_channel'),
@@ -56,9 +74,21 @@ class SynthBA:
 
 
     @torch.inference_mode()
-    def run(self, scan: Union[Nifti1Image,Nifti2Image]) -> float:
+    def run(
+        self, 
+        scan: Union[Nifti1Image,Nifti2Image],
+        preprocess: bool = True,
+        mr_weighting: str = 't1'
+    ) -> float:
         """
         """
+        if mr_weighting not in self.templates.keys():
+            raise Exception('Uknown weighting. Please select between:' + ','.join(self.supported_weightings))
+
+        if preprocess:
+            mri = self.skull_stripping.run(mri)[0]
+            mri = self.align_fn(mri, self.templates[mr_weighting])
+
         scan_tensor = self.transforms_fn_direct(scan.get_fdata())
         scan_tensor = scan_tensor.unsqueeze(0).to(self.device).float()  
         return self.model(scan_tensor).view(-1).item() * 100.
